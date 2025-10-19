15ade54e21a8535213f1f70abd254ea4929555c5
kalilinux <varunbali345@gmail.com>
2025-10-19 16:05:15 +0530

    bfs

--- graph/bfs4.java
+++ graph/bfs4.java
@@ -1,32 +1,32 @@
-
 import java.util.*;
 
-public class bfs3 {
+public class bfs4 {
 
-  public static ArrayList<Integer> bf(ArrayList<ArrayList<Integer>> adj) {
-    int v = adj.size();
-    int s = 2;
-    ArrayList<Integer> res = new ArrayList<>();
-    boolean[] vis = new boolean[v];
-    Queue<Integer> q = new LinkedList<>();
-    vis[s] = true;
+  public static ArrayList<Integer> bf(ArrayList<ArrayList<Integer>> adj){
+
+    int v=adj.size();
+    int s=2;
+    ArrayList<Integer> res=new ArrayList<>();
+    Queue<Integer> q=new LinkedList<>();
+    boolean[] vis=new boolean[v];
+
     q.add(s);
-    while (!q.isEmpty()) {
-      int curr = q.poll();
+    vis[s]=true;
+    while(!q.isEmpty()){
+      int curr=q.poll();
       res.add(curr);
-      for (int x : adj.get(curr)) {
-        if (!vis[x]) {
-          vis[x] = true;
+      for(int x:adj.get(curr)){
+        if(!vis[x]){
+          vis[x]=true;
           q.add(x);
-
         }
       }
     }
     return res;
-
   }
 
   public static void main(String[] args) {
+
     ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
     adj.add(new ArrayList<>(Arrays.asList(1, 2)));
     adj.add(new ArrayList<>(Arrays.asList(0, 2, 3)));
@@ -38,7 +38,6 @@ public class bfs3 {
     for (int i : ans) {
       System.out.print(i + " ");
     }
-
+    System.out.println();
   }
-
 }
