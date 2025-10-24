00fabbeebb2bfadcd45c7ed90063cf47db56160c
kalilinux <varunbali345@gmail.com>
2025-10-24 11:49:50 +0530

    operation on bst

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -37,7 +37,7 @@ public class BST {
     return root;
   }
 
-  public  boolean search(Node root, int data) {
+  public boolean search(Node root, int data) {
     if (root == null) {
       return false;
     }
@@ -50,13 +50,104 @@ public class BST {
     return search(root.right, data);
   }
 
-   public void printInorder(Node node)
-    {
-        if (node == null)
-            return;
 
-        printInorder(node.left);
-        System.out.print(node.data + " ");
-        printInorder(node.right);
+  public int maxheight(Node root){
+    if(root==null){
+      return -1;
     }
+    int leftheight=maxheight(root.left);
+    int rightheight=maxheight(root.right);
+
+    return 1+Math.max(leftheight,rightheight);
+
+  }
+
+  public int iabalancedrec(Node root){
+    if(root==null){
+      return 0;
+    }
+    int leftheight=iabalancedrec(root.left);
+    int rightheight=iabalancedrec(root.right);
+
+    if(leftheight==-1||rightheight==-1||Math.abs(leftheight-rightheight)>1){
+      return -1;
+    }
+
+    return Math.max(leftheight,rightheight)+1;
+  }
+
+  public boolean isbalanced(Node root){
+    return iabalancedrec(root)>0;
+  }
+
+
+  public Node getsucc(Node root){
+    while(root.left!=null){
+      root=root.left;
+    }
+    return root;
+  }
+
+
+  public Node deleteNode(Node root, int valueToDelete) {
+    if (root == null) {
+      return null;
+    }
+    if(valueToDelete<root.data){
+      root.left=deleteNode(root.left,valueToDelete);
+    }
+    else if(valueToDelete>root.data){
+      root.right=deleteNode(root.right,valueToDelete);
+    }
+    else{
+      if(root.left==null&root.right==null){
+        return null;
+      }
+      else if(root.left==null){
+        return root.right;
+      }
+      else if(root.right==null){
+        return root.left;
+      }
+      else{
+        Node succ=getsucc(root.right);
+        root.data=succ.data;
+        root.right=deleteNode(root.right,succ.data);
+      }
+    }
+    return root;
+  }
+
+  public int minvalue(Node root) {
+    if (root == null) {
+      return -1;
+    }
+    Node curr = root;
+    while (curr.left != null) {
+      curr = curr.left;
+    }
+    return curr.data;
+  }
+
+  public int maxvalue(Node root) {
+    if (root == null) {
+      return -1;
+    }
+    Node curr = root;
+    while (curr.right != null) {
+      curr = curr.right;
+    }
+    return curr.data;
+  }
+
+
+
+  public void printInorder(Node node) {
+    if (node == null)
+      return;
+
+    printInorder(node.left);
+    System.out.print(node.data + " ");
+    printInorder(node.right);
+  }
 }
--- BinarySearchTree/example/binaryExample.java
+++ BinarySearchTree/example/binaryExample.java
@@ -3,24 +3,30 @@ package BinarySearchTree.example;
 import BinarySearchTree.BST;
 import BinarySearchTree.Node;
 
-
 public class binaryExample {
-  public static void main(String[] args){
-    BST obj=new BST();
-    Node root=null;
-    root=obj.insert(root,20);
-    root=obj.insert(root,8);
-    root=obj.insert(root,22);
-    root=obj.insert(root,4);
-    root=obj.insert(root,12);
-    root=obj.insert(root,10);
-    root=obj.insert(root,14);
+  public static void main(String[] args) {
+    BST obj = new BST();
+    Node root = null;
+    root = obj.insert(root, 20);
+    root = obj.insert(root, 8);
+    root = obj.insert(root, 22);
+    root = obj.insert(root, 4);
+    root = obj.insert(root, 12);
+    root = obj.insert(root, 10);
+    root = obj.insert(root, 14);
 
+    // obj.printInorder(root);
+
+    // obj.deleteNode(root, 8);
+    // System.out.println(obj.maxheight(root));
+    // System.out.println(obj.minvalue(root));
+    // System.out.println(obj.maxvalue(root));
+    System.out.println(obj.isbalanced(root));
     obj.printInorder(root);
     System.out.println();
-    obj.levelorder(root);
-    System.out.println();
-    System.out.println(obj.search(root,1));
+    // obj.levelorder(root);
+    // System.out.println();
+    // System.out.println(obj.search(root,1));
 
   }
 }
